f11fceb332f1668c3940e44740a09c4b9720ba45
Halil Özcan <halil_ozcann@outlook.com>
2024-12-18 22:26:19 +0300

    C# Koleksiyon Hash Table Yapıldı.

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,24 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
+            Hashtable H1 = new Hashtable();
+
+            H1.Add("Car", "Araba");
+            H1.Add("House", "Ev");
+
+            // yardımcı metotlar
+            bool kontrol = H1.Contains("House");
+
+            H1.ContainsKey("House");
+            H1.ContainsValue("Ev");
+
+            H1.Clear();
+
+            int KoleksiyonCount = H1.Count; // Koleksiyon içindeki toplamm değeri verir;
+
+            H1.Remove("Car");
+
+            H1["House"] = "Villa"; // Güncellemek istenen deger
         }
     }
 }
